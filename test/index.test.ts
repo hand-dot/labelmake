852544ba737bcdf5068fbe6da1c6b1f855557043
hand-dot <kyouhei.fukuda0729@gmail.com>
2020-03-03 19:45:29 +0900

    add ut

--- test/index.test.ts
+++ test/index.test.ts
@@ -4,59 +4,92 @@ import { sans_vfs_fonts } from "./fonts/sans_vfs_fonts";
 import { serif_vfs_fonts } from "./fonts/serif_vfs_fonts";
 import fs from "fs";
 import Labelmake from "../src/index";
+const PDFParser = require("pdf2json");
 import { TemplateData } from "../src/type";
 
-describe("test", () => {
+const getPdf = (pdfFilePath: string) => {
+  const pdfParser = new PDFParser();
+  return new Promise((resolve, reject) => {
+    pdfParser.on("pdfParser_dataError", reject);
+    pdfParser.on("pdfParser_dataReady", resolve);
+    pdfParser.loadPDF(pdfFilePath);
+  });
+};
+
+const getTmpPath = (fileName: string) => __dirname + `/tmp/${fileName}`;
+
+const getTemplateData = (): TemplateData => ({
+  image: null,
+  position: {
+    test: {
+      position: { x: 10, y: 10 },
+      width: 20,
+      alignment: "left",
+      size: 8,
+      space: 0,
+      type: "text",
+      lineHeight: 1
+    }
+  },
+  pageSize: {
+    width: 100,
+    height: 100
+  },
+  fontName: ""
+});
+
+describe("Labelmake integrate test", () => {
+  afterAll(() => {
+    const dir = __dirname + "/tmp";
+    fs.readdir(dir, (err, files) => {
+      if (err) {
+        throw err;
+      }
+      files.forEach(file => {
+        if (file !== ".gitkeep") {
+          fs.unlink(`${dir}/${file}`, err => {
+            if (err) {
+              throw err;
+            }
+          });
+        }
+      });
+    });
+  });
+
   test("NotoSansCJKjp", async () => {
+    const fontName = "NotoSansCJKjp";
     const labelmake = new Labelmake();
     labelmake.registerFont("NotoSansCJKjp", sans_vfs_fonts);
-    const templateData: TemplateData = {
-      image: null,
-      position: {
-        test: {
-          position: { x: 10, y: 10 },
-          width: 20,
-          alignment: "left",
-          size: 8,
-          space: 0,
-          type: "text",
-          lineHeight: 1
-        }
-      },
-      pageSize: {
-        width: 100,
-        height: 100
-      },
-      fontName: "NotoSansCJKjp"
-    };
-    const pdf = await labelmake.create(templateData, [{ test: "hello" }]);
-    // TODO PDFのアサーションとファイルの削除
-    fs.writeFileSync("./sans.pdf", pdf);
+    const pdf = await labelmake.create(
+      Object.assign(getTemplateData(), { fontName }),
+      [{ test: "hello" }]
+    );
+    const file = getTmpPath("sans.pdf");
+    fs.writeFileSync(file, pdf);
+    const ress = await Promise.all([
+      getPdf(file),
+      getPdf(__dirname + "/assert/sans.pdf")
+    ]);
+    const [a, e] = ress;
+    expect(a).toEqual(e);
   });
+
   test("NotoSerifCJKjp", async () => {
+    const fontName = "NotoSerifCJKjp";
     const labelmake = new Labelmake();
     labelmake.registerFont("NotoSerifCJKjp", serif_vfs_fonts);
-    const templateData: TemplateData = {
-      image: null,
-      position: {
-        test: {
-          position: { x: 10, y: 10 },
-          width: 20,
-          alignment: "left",
-          size: 8,
-          space: 0,
-          type: "text",
-          lineHeight: 1
-        }
-      },
-      pageSize: {
-        width: 100,
-        height: 100
-      },
-      fontName: "NotoSerifCJKjp"
-    };
-    const pdf = await labelmake.create(templateData, [{ test: "hello" }]);
-    // TODO PDFのアサーションとファイルの削除
-    fs.writeFileSync("./serif.pdf", pdf);
+    const pdf = await labelmake.create(
+      Object.assign(getTemplateData(), { fontName }),
+      [{ test: "hello" }]
+    );
+    const file = getTmpPath("serif.pdf");
+    fs.writeFileSync(file, pdf);
+    const ress = await Promise.all([
+      getPdf(file),
+      getPdf(__dirname + "/assert/serif.pdf")
+    ]);
+    const [a, e] = ress;
+    expect(a).toEqual(e);
   });
 });
