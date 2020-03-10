c9363c13914167bd5a783821a206ce0625b4ae74
hand-dot <kyouhei.fukuda0729@gmail.com>
2020-03-10 10:27:20 +0900

    fix rollupconfig

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,8 +1,6 @@
-import resolve from "rollup-plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
-import sourceMaps from "rollup-plugin-sourcemaps";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
-import json from "rollup-plugin-json";
 
 const pkg = require("./package.json");
 
@@ -18,33 +16,14 @@ export default {
       sourcemap: true,
       compact: true,
       freeze: false
-    },
-    {
-      file: pkg.module,
-      format: "es",
-      sourcemap: true,
-      compact: true,
-      freeze: false
     }
   ],
-  // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: [],
   watch: {
     include: "src/**"
   },
   plugins: [
-    // Allow json resolution
-    json(),
-    // Compile TypeScript files
     typescript({ useTsconfigDeclarationDir: true }),
-    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
     commonjs(),
-    // Allow node_modules resolution, so you can use 'external' to control
-    // which external modules to include in the bundle
-    // https://github.com/rollup/rollup-plugin-node-resolve#usage
-    resolve(),
-
-    // Resolve source maps to the original source
-    sourceMaps()
+    resolve()
   ]
 };
