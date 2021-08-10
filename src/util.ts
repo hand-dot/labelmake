2ce0a4bb464fff7cd0ee367124f3bbcd3863e840
Tilman Marquart <til.marquart@gmail.com>
2021-08-10 10:40:40 +0200

    feat: split text on last whitespace bevor overlap

--- src/util.ts
+++ src/util.ts
@@ -37,5 +37,62 @@ export const calcX = (
     return mm2pt(x) + addition;
   };
   
-  export const calcY = (y: number, height: number, itemHeight: number) =>
+export const calcY = (y: number, height: number, itemHeight: number) =>
     height - mm2pt(y) - itemHeight;
+
+
+type IsOverEval = (testString: string) => boolean
+/**
+ * Incrementally check the current line for it's real length 
+ * and return the position where it exceeds the bbox width.
+ * 
+ * return `null` to indicate if inputLine is shorter as the available bbox
+ */
+export const getOverPosition = (inputLine: string, isOverEval: IsOverEval) => {
+  for(let i=0; i <= inputLine.length; i++){
+      if(isOverEval(inputLine.substr(0,i))){
+          return i
+      }
+  }
+  return null;
+}
+
+/**
+* Get position of the split. Split the exceeding line at 
+* the last whitepsace bevor it exceeds the bounding box width.
+*/
+const getSplitPosition = (inputLine: string, isOverEval: IsOverEval) => {
+  const overPos = getOverPosition(inputLine, isOverEval)
+  console.log('overPos', inputLine, overPos)
+  /**
+   * if input line is shorter as the available space. We split at the end of the line
+   */
+  if(overPos === null) return inputLine.length;
+  let overPosTmp = overPos
+  while(inputLine[overPosTmp] !== " " && overPosTmp >= 0) overPosTmp--;
+  /**
+   * for very long lines with no whitespace use the original overPos and 
+   * split one char bevor so we do not overfill the bbox
+   */
+  return overPosTmp > 0 ? overPosTmp : overPos - 1;
+}
+
+/** 
+* recursivly split the line at getSplitPosition. 
+* If there is some leftover, split the rest again in the same manner.
+*/
+export const getSplittedLines = (inputLine: string, isOverEval: IsOverEval): string[] => {
+  const splitPos = getSplitPosition(inputLine, isOverEval)
+  console.log('splitPos', inputLine, splitPos)
+  const splittedLine = inputLine.substr(0, splitPos)
+  const rest = inputLine.slice(splitPos).trimLeft()
+  /**
+   * end recursion if there is no rest, return single splitted line in an array
+   * so we can join them over the recursion
+   */
+  if(rest.length === 0){
+      return [splittedLine];
+  } else {
+      return [splittedLine, ...getSplittedLines(rest, isOverEval)]
+  }
+}
