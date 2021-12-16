656646c600db7fd1c529f9ff50f6ea1792163fbe
Kyohei Fukuda <kyouhei.fukuda0729@gmail.com>
2021-12-16 09:29:47 +0900

    Develop (#60)
    * Support utf8 characters in qrcode and decode qrcode image test (#57)
    
    * support utf8 characters in qrcode and testing decoded value
    
    * remove unused npm package
    
    * add testcase about katakana
    
    * minor fix
    
    * update node version in ci unit testing
    
    Co-authored-by: fukata <tatsuya.fukata@gmail.com>

--- test/barcode.test.ts
+++ test/barcode.test.ts
@@ -1,4 +1,6 @@
-import { validateBarcodeInput } from "../src/barcode";
+import { validateBarcodeInput, createBarCode } from "../src/barcode";
+import jsQR, { QRCode } from "jsqr";
+import { PNG } from "pngjs";
 
 describe("validateBarcodeInput", () => {
   test("qrcode", () => {
@@ -173,5 +175,40 @@ describe("validateBarcodeInput", () => {
     expect(validateBarcodeInput(type, invalid1)).toEqual(false);
     expect(validateBarcodeInput(type, invalid2)).toEqual(false);
     expect(validateBarcodeInput(type, blank)).toEqual(false);
+  });
+});
+
+/**
+ * 生成したQRコード（png）画像から入力データが正常に読み取れるかをテスト
+ */
+describe("createBarCode", () => {
+  describe("qrcode", () => {
+    // テスト名, input, expected
+    const tests = [
+      ["URL", "https://www.google.com/", "https://www.google.com/"],
+      ["ひらがな", "てすとです", "てすとです"],
+      ["ひらがな2", "あいうえおあいうえお２", "あいうえおあいうえお２"],
+      ["カタカナ", "テストです", "テストです"],
+      ["漢字", "お正月", "お正月"],
+      ["中国語", "新年快乐", "新年快乐"],
+      ["タイ語", "สวัสดีปีใหม่", "สวัสดีปีใหม่"],
+    ];
+    for (const t of tests) {
+      test(`${t[0]}: ${t[1]}`, async () => {
+        const buffer = (await createBarCode({
+          type: "qrcode",
+          input: t[1],
+          width: 10, // mm
+          height: 10, // mm
+          backgroundColor: "00000000", // 背景色を指定しないとjsQRでうまく解析できない
+        })) as Buffer;
+        const png = PNG.sync.read(buffer);
+        const pngData = new Uint8ClampedArray(png.data);
+        const qr = jsQR(pngData, png.width, png.height) as QRCode;
+        expect(qr).not.toBeNull();
+        const dataBuffer = Buffer.from(qr.binaryData);
+        expect(dataBuffer.toString("utf8")).toEqual(t[2]);
+      });
+    }
   });
 });
